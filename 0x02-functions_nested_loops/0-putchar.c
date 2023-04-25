aceafcf8d86cf884454427fcd35c146d3606210c
wafaa minyaoui <lailaminyaoui@example.com>
2023-04-25 07:36:52 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * main - entry point
- * description: 'prints _putchar to stdout'
- * Return: always 0
- */
+* main - entry point
+* description: 'prints _putchar to stdout'
+* Return: always 0
+*/
 int main(void)
 {
 _putchar('_');
@@ -15,4 +15,5 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
+return (0);
 }
