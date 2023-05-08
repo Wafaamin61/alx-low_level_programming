3bdf5d59e09e2a5344ae0bf0a768466c5125948a
wafaa minyaoui <lailaminyaoui@example.com>
2023-05-08 13:57:44 -0700

    hello

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,4 +1,4 @@
-i#ifndef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 
 char *_memset(char *s, char b, unsigned int n);
@@ -11,4 +11,6 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
+
 #endif
+
